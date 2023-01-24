b88653a3824eaec2c166ed721604af6d3f1b3608
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-01-24 09:47:05 +0530

    added home and header component

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import "./styles/app.scss";
-
-function App() {
-  return;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./styles/app.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
